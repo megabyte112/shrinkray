99243068149d5ac5a2a4ca993e101d1289a73efb
megabyte112 <74556753+megabyte112@users.noreply.github.com>
2022-07-26 22:33:55 +0100

    meme mode, target fps

--- shrinkray.py
+++ shrinkray.py
@@ -7,51 +7,7 @@
 
 # Here you can find advanced settings for shrinkray.
 # Lines prefixed with a hash (#) are comments and are ignored by code.
-
-## -----[Video]----- ##
-
-# maximum allowed size of longest edge (in pixels) before scaling is needed.
-# default: 1280
-max_res_size = 1280
-
-# highest allowed audio bitrate (in kbps).
-# ignored when audioonly is True
-# set to None to disable.
-# default: 256
-max_audio_bitrate = 256
-
-# container to contain video files.
-# default: "mp4"
-container = "mp4"
-
-# video codec to use
-# default: "libx264"
-video_codec = "libx264"
-
-## -----[Audio]----- ##
-
-# don't include audio. 
-# doesn't work when audioonly is True
-# default: False
-mute = False
-
-# fraction of bandwith dedicated to audio.
-# ignored when mute or audioonly is True
-# must be less than 1.
-# default: 1/4
-audioratio = 1/4
-
-# don't include video, and store file in audio container.
-# default: False
-audioonly = False
-
-# container to store audio when audioonly is True.
-# default: "mp3"
-audiocontainer = "mp3"
-
-# audio codec to use
-# default: "libmp3lame"
-audio_codec = "libmp3lame"
+# All booleans (True/False) must have the first letter capitalized.
 
 ## ----[General]---- ##
 
@@ -68,7 +24,82 @@ suffix = "_shrinkray"
 # default: 0.95
 bitrate_multiplier = 0.95
 
-### ---------------------------------- ###
+# ask user whether audioonly shall be used each launch.
+# this is useless when audioonly is already True.
+# user is always asked this if using a local file.
+# default: False
+ask_audio = False
+
+# ask user for target file size each launch.
+# default: False
+ask_size = False
+
+# default target file size in MB.
+# default: 8
+default_size = 8
+
+## -----[Video]----- ##
+
+# maximum allowed size of longest edge (in pixels) before scaling is needed.
+# default: 1280
+max_res_size = 1280
+
+# highest allowed audio bitrate (in kbps).
+# ignored when audioonly is True.
+# set to None to disable.
+# default: 256
+max_audio_bitrate = 256
+
+# framerate limit.
+# default: 30
+target_fps = 30
+
+# container to contain video files.
+# default: "mp4"
+container = "mp4"
+
+# video codec to use.
+# default: "libx264"
+video_codec = "libx264"
+
+## -----[Audio]----- ##
+
+# don't include audio. 
+# doesn't work when audioonly is True.
+# default: False
+mute = False
+
+# fraction of bandwith dedicated to audio.
+# ignored when mute or audioonly is True.
+# must be less than 1.
+# default: 1/4
+audioratio = 1/4
+
+# don't include video, and store file in audio container.
+# default: False
+audioonly = False
+
+# container to store audio when audioonly is True.
+# default: "mp3"
+audiocontainer = "mp3"
+
+# audio codec to use.
+# default: "libmp3lame"
+audio_codec = "libmp3lame"
+
+## ------[Fun]------ ##
+
+# ultra high compression for those moldy shitposts.
+# locks video bitrate to 20kbps, and audio bitrate to 10kbps.
+# resolution is also smaller and FPS is capped at 10.
+# default: False
+meme_mode = False
+
+### -------[~~End of Settings~~]------- ###
+
+# Everything below this point is actual code.
+# Only edit if you know exactly what you're doing.
+
 
 
 # okay, let's do this.
@@ -104,7 +135,7 @@ if shutil.which("ffmpeg") is None or shutil.which("yt-dlp") is None or shutil.wh
     print("Download yt-dlp here: https://github.com/yt-dlp/yt-dlp/releases")
     print()
     print("Make sure to add the folder to path!")
-    input()
+    input("When you're done, reopen shrinkray, and you're all set.")
     sys.exit()
 
 # don't edit
@@ -126,6 +157,9 @@ if os.path.exists("logs/shrinkray.log"):
 logformat='%(asctime)s: %(message)s'
 logging.basicConfig(filename="logs/shrinkray.log", filemode="w", level=logging.INFO, format=logformat)
 
+if verbose:
+    logging.getLogger().addHandler(logging.StreamHandler())
+
 logging.info(f"shrinkray {version} is running")
 logging.info("tell megabyte112 about any issues!!")
 logging.info("args: " + str(sys.argv))
@@ -133,6 +167,7 @@ logging.info("mute: "+str(mute))
 logging.info("audioonly: "+str(audioonly))
 logging.info("audioratio: "+str(audioratio))
 logging.info("bitrate_multiplier: "+str(bitrate_multiplier))
+logging.info("meme mode: "+str(meme_mode))
 
 # determine host OS
 if os.name == "nt": # windows
@@ -142,12 +177,18 @@ else:   # posix
     logging.info("host is non-Windows")
     nullfile="/dev/null"
 
-if mute:
-    print("WARNING: Video will be muted!")
-if audioonly:
-    print("WARNING: Output will be audio only!")
+# we're running!!
+print(f"Welcome back to shrinkray, version {version}")
 
-print("Welcome back to shrinkray, version "+version)
+if meme_mode:
+    ask_size = False
+    ask_audio = False
+    print("\nhaha shitpost shrinkray go brrrrrrrrr")
+    print("(meme mode is active)")
+elif audioonly:
+    print("\nWARNING: Output will be audio only!")
+elif mute:
+    print("\nWARNING: Video will be muted!")
 
 # text must be a number greater than 0
 def CheckValidInput(text):
@@ -163,6 +204,9 @@ def GetTargetSize():
             print("\nMake sure your input a whole number greater than 0")
     return target_size
 
+def GetAudioChoice():
+    return input("\nGet audio only? [Y/N]\n> ").lower() == "y"
+
 # yt-dlp will get this
 dlcontain = "mp4"
 
@@ -172,7 +216,13 @@ if arg_length < 2:
     logging.info("prepare download...")
     url=input("\nPaste a video link here.\nIt can be YouTube, Reddit, and most other video sites.\n> ")
     logging.info(f"input: \"{url}\"")
-    target_size = GetTargetSize()
+    if ask_size:
+        target_size = GetTargetSize()
+    else:
+        target_size = default_size
+    if ask_audio:
+        audioonly = GetAudioChoice()
+        logging.info("audioonly: "+str(audioonly))
     print("\nFetching video...")
     titlecmd = "yt-dlp -e --no-playlist "+url
     logging.info("fetching title with the following command")
@@ -181,10 +231,10 @@ if arg_length < 2:
     if p[0] != 0:
         logging.info("title grab failed with return code "+str(p[0]))
         print("There was an issue with your video URL.\nClose this window and run shrinkray again.")
-        print("Be sure to check whether your URL is correct!")
-        input()
+        logging.shutdown()
+        input("Be sure to check whether your URL is correct!")
         sys.exit()
-    title = p[1]
+    title = p[1].replace("\"","'")
     filein = "download/"+str(title)+"."+dlcontain
     logging.info("title: "+str(title))
     url=url.replace("\"","\\\"")
@@ -202,7 +252,12 @@ if arg_length < 2:
 else:
     filein=sys.argv[1]
     logging.info("target file "+filein)
-    target_size = GetTargetSize()
+    if ask_size:
+        target_size = GetTargetSize()
+    else:
+        target_size = default_size
+    if not meme_mode:
+        audioonly = GetAudioChoice()
 
 if audioonly:
     container = audiocontainer
@@ -218,7 +273,6 @@ if fileincontain != container:
     logging.info(convertcommand)
     os.system(convertcommand)
     filein = filenocontain+"."+container
-
 
 # figure out valid file name
 fullname = filein.split("/")
@@ -239,13 +293,14 @@ logging.info("target size: "+str(targetSizeKB)+"KB")
 # calculate size: no need to shrink if file is already small enough
 size=os.path.getsize(filein)/1024   # in kiB
 logging.info(f"size of input file: {size}kiB")
-if size < targetSizeKB:
+if size < targetSizeKB and not meme_mode:
     logging.info("file is already small enough")
     newsize=size
     print("\nThe file is already small enough!")
     shutil.copy(filein, fileout)
     print("It has been copied to the output folder.")
     logging.info("complete!")
+    logging.shutdown()
     input("You can now close this window.")
     sys.exit()
 
@@ -257,11 +312,24 @@ logging.info(lencmd)
 length = math.ceil(float(subprocess.getoutput(lencmd)))
 logging.info(f"video length: {length}s")
 totalbitrate=math.floor(targetSizeKB*8/length)
-logging.info(f"total bitrate: {totalbitrate}kbps")
+
+# get fps
+if not audioonly:
+    if meme_mode:
+        target_fps = 10
+    fpscmd = f"ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate \"{filein}\""
+    logging.info("fetching framerate with the following command")
+    logging.info(fpscmd)
+    fullfps = subprocess.getoutput(fpscmd).split("/")
+    fps = float(int(fullfps[0])/int(fullfps[1]))
+    logging.info(f"video is {fps}fps")
+    lowerfps = fps > target_fps
 
 # get resolution
 doScale = False
 if not audioonly:
+    if meme_mode:
+        max_res_size = 640
     rescmd = f"ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 \"{filein}\""
     logging.info("fetching resolution with the following command")
     logging.info(rescmd)
@@ -292,8 +360,16 @@ else:
     if max_audio_bitrate is not None and audiobitrate > max_audio_bitrate:
         audiobitrate = max_audio_bitrate
     videobitrate=totalbitrate-audiobitrate
+
+# ahh yes, crusty shitposts
+if meme_mode:
+    audiobitrate = 10
+    videobitrate = 20
+    totalbitrate = audiobitrate + videobitrate
+
 logging.info(f"audio bitrate: {audiobitrate}kbps")
 logging.info(f"video bitrate: {videobitrate}kbps")
+logging.info(f"total bitrate: {totalbitrate}kbps")
 
 audioargs = f"-c:a {audio_codec} -b:a {audiobitrate}k"
 if mute:
@@ -312,13 +388,17 @@ if audioonly:
 
 else:
     if doScale:
+        if lowerfps:
+            fpsargs = ",fps="+str(target_fps)
+        else:
+            fpsargs = ""
         if orientation == 'p':
             scale = max_res_size / height
-            newres = str(round(width*scale))+":"+str(max_res_size)
+            newres = str(round((width*scale)/2)*2)+":"+str(max_res_size)
         else:
             scale = max_res_size / width
-            newres = str(max_res_size)+":"+str(round(height*scale))
-        videoargs = f"-vf scale={newres},fps=30 -c:v {video_codec} -b:v {videobitrate}k"
+            newres = str(max_res_size)+":"+str(round((height*scale)/2)*2)
+        videoargs = f"-vf scale={newres}{fpsargs} -c:v {video_codec} -b:v {videobitrate}k"
         if audioonly:
             videoargs = ""
         if verbose:
@@ -328,7 +408,11 @@ else:
             ffmpeg_commands = [f"ffpb -y -hide_banner -i \"{filein}\" {videoargs} -passlogfile logs/fflog -pass 1 -f null {nullfile}",
             f"ffpb -y -hide_banner -i \"{filein}\" {videoargs} -passlogfile logs/fflog {audioargs} -pass 2 \"{fileout}\""]
     else:
-        videoargs = f"-vf fps=30 -c:v {video_codec} -b:v {videobitrate}k"
+        if lowerfps:
+            fpsargs = "-vf fps="+str(target_fps)
+        else:
+            fpsargs = ""
+        videoargs = f"{fpsargs} -c:v {video_codec} -b:v {videobitrate}k"
         if verbose:
             ffmpeg_commands = [f"ffpb -y -i \"{filein}\" {videoargs} -passlogfile logs/fflog -pass 1 -f null {nullfile}",
             f"ffpb -y -i \"{filein}\" {videoargs} -passlogfile logs/fflog {audioargs} -pass 2 \"{fileout}\""]
@@ -346,7 +430,6 @@ else:
     os.system(ffmpeg_commands[1])
     logging.info("called both commands")
 
-
 # done!
 print("\nShrinking complete!\nCheck the output folder for your file.")
 newsize=os.path.getsize(fileout)/1000
@@ -357,8 +440,10 @@ expected = (targetSizeKB/1000)*1024
 print(f"\nCompressed {displaysize}kB into {newdisplaysize}kB\n")
 if newdisplaysize > expected:
     logging.info("file is larger than expected!")
-    print("It looks like shrinkray couldn't shrink your file as much as expected.")
-    print("If you need it to be smaller, try running shrinkray again and lowering the target size.")
+    print("It looks like shrinkray couldn't shrink your file as much as you requested.")
+    print("If you need it to be smaller, try lowering the target size and running shrinkray again.")
+    print("The file was still saved.")
 logging.info("complete!")
+logging.shutdown()
 input("You can now close this window.")
 sys.exit()
